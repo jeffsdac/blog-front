6c3a93811e1f33a9a9bf0025b6e0d539c4bca09a
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2026-05-05 11:48:06 -0300

    refactor: correct folder responsability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { Login } from './features/login/login';
 
 export const routes: Routes = [
-    { path:'login', component: Login }
+    { path:'', component: Login }
 ];
--- src/app/features/login/pages/login.ts
+++ src/app/features/login/pages/login.ts
@@ -4,6 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+
+  
+}
