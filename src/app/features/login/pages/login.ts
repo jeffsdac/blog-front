08b0ae8356a0412ee9faeb29589c37ef87ac6555
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2026-05-06 10:42:12 -0300

    feat: enhance login form with validation and error handling

--- src/app/features/login/pages/login.ts
+++ src/app/features/login/pages/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ILoginDTO } from '../models/ilogin-dto';
+import { form, required, min, minLength, maxLength, FormField } from '@angular/forms/signals';
+import { LoginService } from '../service/login.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormField],
   templateUrl: './login.html',
 })
 export class Login {
+  protected readonly loginService = inject(LoginService);
 
-  
+  loginModel = signal<ILoginDTO>({
+    username: '',
+    password: ''
+  });
+
+  LoginModelForm = form(this.loginModel, (schemaPath) =>{
+    required( schemaPath.username, { message: "Campo obrigatório" } ),
+    minLength( schemaPath.username, 5, { message: "Tamanho mínimo é de 5 caracteres" } ),
+    maxLength( schemaPath.username , 100, { message: "Tamanho máximo é de 100 caractéres" } ),
+
+    required( schemaPath.password, { message:  "Campo obrigatório"} ),
+    minLength( schemaPath.password, 10 , {message: "Senha precisa ter pelo menos 10 caractéres" } ),
+    maxLength( schemaPath.password, 300, { message: "Senha pode ter no máximo 300 caractéres" } )
+  }) 
+
+  onSubmit ( event: Event ) {
+    event.preventDefault();
+    this.loginService.doLogin(this.loginModel());
+  }
+
 }
