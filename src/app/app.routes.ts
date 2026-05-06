0ed630252a04d12366023c3608a9fa84c451472e
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2026-05-06 11:08:58 -0300

    fix: correct import path for Login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Login } from './features/login/login';
+import { Login } from './features/login/pages/login';
 
 export const routes: Routes = [
     { path:'', component: Login }
