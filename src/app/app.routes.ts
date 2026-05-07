83c8be82b46e53be931376f9d9973b4d37f0b4d3
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2026-05-07 15:12:55 -0300

    feat: add Homepage component with routing and basic template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { Login } from './features/login/pages/login';
 import { Register } from './features/user/components/register/register';
 import { AuthLayout } from './features/auth/layout/auth-layout';
+import { Homepage } from './features/post/page/homepage/homepage';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '', component: Homepage },
     {
         path: '',
         component: AuthLayout,
