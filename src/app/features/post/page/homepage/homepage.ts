a3c5e45dd726fd59a559b2d1855b5ae70e84b686
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2026-05-12 08:41:07 -0300

    refactor: refacotr sidebar component into shared and integrate into homepage layout

--- src/app/features/post/page/homepage/homepage.ts
+++ src/app/features/post/page/homepage/homepage.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Header } from '../../../../shared/header/header';
 import { GetPostService } from '../../service/get-post-service';
 import { PostCard } from '../../component/post-card/post-card';
+import { Sidebar } from '../../../../shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-homepage',
-  imports: [Header, PostCard],
+  imports: [Header, Sidebar, PostCard],
   templateUrl: './homepage.html',
 })
 export class Homepage implements OnInit{
